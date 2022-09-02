3d1886eb0cf8b89e13f73b14b314b93a2983dff5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-09-02 00:49:53 -0700

    server.js updated, mongoose.connect localhost, app.use express middleware

---
+++ server.js
@@ -0,0 +1,31 @@
+// require mongoose
+const mongoose = require('mongoose');
+// require express
+const express = require('express');
+// require config connection
+const db = require('./config/connections');
+// require routes/api
+const routes = require('./routes');
+// use app as express method
+const app = express();
+// PORT env
+const PORT = process.env.PORT || 3001;
+
+// use express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(routes);
+
+// connect mongoose to localhost
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nosql-social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParse: true,
+    useUnifiedTopology: true
+});
+
+// mongoose debug query
+mongoose.set('debug', true);
+
+// listen PORT console log
+app.listen(PORT, () => console.log(`Connected to Localhost: listening at ${PORT}`));
